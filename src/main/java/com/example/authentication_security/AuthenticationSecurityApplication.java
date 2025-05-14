f7c0ad90bb67098627110324d4bd74da074de2b2
Kwon042 <miri9612@gmail.com>
2025-05-14 10:57:49 +0900

    초기 세팅

---
+++ src/main/java/com/example/authentication_security/AuthenticationSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.authentication_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authentication_security/AuthenticationSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authentication_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
